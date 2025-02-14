2b21b1669ca4a48062b523c558c2a5fd5ce87099
Vckynando12 <vickynando12@gmail.com>
2025-02-15 00:23:26 +0700

    Fix Firebase to use env variable

--- app/Providers/FirebaseServiceProvider.php
+++ app/Providers/FirebaseServiceProvider.php
@@ -13,8 +13,8 @@ class FirebaseServiceProvider extends ServiceProvider
     {
         $this->app->singleton(FirebaseAuth::class, function ($app) {
             $factory = (new Factory)
-                ->withServiceAccount(base_path(env('FIREBASE_CREDENTIALS')))
-                ->withDatabaseUri(env('FIREBASE_DATABASE_URL'));
+            ->withServiceAccount(json_decode(env('FIREBASE_CREDENTIALS'), true))
+            ->withDatabaseUri(env('FIREBASE_DATABASE_URL'));
 
             return $factory->createAuth();
         });
