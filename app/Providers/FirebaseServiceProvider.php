7317d38c09c1bb0d8157bd6c15047242687063aa
Vckynando12 <vickynando12@gmail.com>
2025-02-15 01:22:54 +0700

    update

--- app/Providers/FirebaseServiceProvider.php
+++ app/Providers/FirebaseServiceProvider.php
@@ -14,6 +14,8 @@ class FirebaseServiceProvider extends ServiceProvider
         $this->app->singleton(FirebaseAuth::class, function ($app) {
             $factory = (new Factory)
             ->withServiceAccount(json_decode(env('FIREBASE_CREDENTIALS'), true))
+            ->withDatabaseUri(env('FIREBASE_DATABASE_URL'))
+            ->withServiceAccount(base_path('storage/app/firebase/smartcab-8bb42-firebase-adminsdk-fbsvc-de33a8e45b.json'))
             ->withDatabaseUri(env('FIREBASE_DATABASE_URL'));
 
             return $factory->createAuth();
@@ -22,7 +24,10 @@ class FirebaseServiceProvider extends ServiceProvider
         $this->app->singleton(Database::class, function ($app) {
             $factory = (new Factory)
                 ->withServiceAccount(config('firebase.credentials'))
-                ->withDatabaseUri(config('firebase.database_url'));
+                ->withDatabaseUri(config('firebase.database_url'))
+                ->withServiceAccount(base_path('storage/app/firebase/smartcab-8bb42-firebase-adminsdk-fbsvc-de33a8e45b.json'))
+                ->withDatabaseUri(env('FIREBASE_DATABASE_URL'));
+
 
             return $factory->createDatabase();
         });
